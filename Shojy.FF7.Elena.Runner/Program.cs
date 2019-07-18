af6a444b72b7e0455d41d85196371d4d88ddecda
Joshua Moon <joshua@moon.uk.net>
2019-07-18 15:32:42 +0100

    LGP Reading added/ Can extract a named file from an LGP archive to Stream

--- Shojy.FF7.Elena.Runner/Program.cs
+++ Shojy.FF7.Elena.Runner/Program.cs
@@ -7,11 +7,23 @@ namespace Shojy.FF7.Elena.Runner
     {
         static void Main(string[] args)
         {
-            var path =
-                @"C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VII\data\lang-en\kernel\";
-            var reader = new KernelReader(Path.Combine(path, "KERNEL.BIN"), KernelType.KernelBin)
-                .MergeKernel2Data(Path.Combine(path, "kernel2.bin"));
+            //    var path =
+            //        @"C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VII\data\lang-en\kernel\";
+            //var reader = new KernelReader(Path.Combine(path, "KERNEL.BIN"))
+            //    .MergeKernel2Data(Path.Combine(path, "kernel2.bin"));
 
+
+            var lgp = new LgpReader(@"C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VII\data\menu\menu_us.lgp");
+
+            foreach (var listFile in lgp.ListFiles())
+            {
+                Console.WriteLine(listFile);
+            }
+
+            using (var dataStream = lgp.ExtractFile("yufi.tex"))
+            {
+                // Save or convert file
+            }
             Console.Write("Press any key to exit... ");
             Console.ReadKey();
 
